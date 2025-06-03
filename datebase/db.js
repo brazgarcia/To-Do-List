6599d72c70f3c27fd9ba3214debcab25771ae703
Braz da Silva Garcia <bdanielgarcia@hotmail.com>
2025-06-02 22:29:45 -0300

    Atualização da estrutura do projeto

---
+++ datebase/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const connectToDb = () => {
+    mongoose.connect (
+        "mongodb+srv://root:admin@to-do-list.q4naqjn.mongodb.net/?retryWrites=true&w=majority&appName=To-do-List", 
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    ).then(() => {
+        console.log("MongoDB Atlas CONECTADO!");
+    }).catch((err) => {
+        console.log(err);
+    });
+};
+
+module.exports = connectToDb;
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const routes = require("express").Router();
 const taskController = require("../controller/TaskController");
 
-routes.get("/home", taskController.getAll);
+routes.get("/home", taskController.getAllTasks);
 
 module.exports = routes;
