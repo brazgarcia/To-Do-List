27d6c3c9e48cc53e126ac0db1c044076f1d671c8
Braz da Silva Garcia <bdanielgarcia@hotmail.com>
2025-07-09 19:23:16 -0300

    Project finish

--- database/db.js
+++ database/db.js
@@ -3,11 +3,7 @@ const mongoose = require("mongoose");
 const connectToDb = () => {
   mongoose
     .connect(
-      "mongodb+srv://root:admin@to-do-list.q4naqjn.mongodb.net/?retryWrites=true&w=majority&appName=To-do-List",
-      {
-        useNewUrlParser: true, // Uso obsoleto 
-        useUnifiedTopology: true, // Uso obsoleto 
-      }
+      process.env.DB_URI,
     )
     .then(() => console.log("MongoDB Atlas CONECTADO!"))
     .catch((err) => console.log(err));
